5afb1b88a71291551de11c4d1c48a1b91a1fbf80
Paul <paullangton4@gmail.com>
2016-11-01 13:35:28 -0400

    added console confirmations

--- src/multiplication.c
+++ src/multiplication.c
@@ -6,6 +6,7 @@
 
 int main (int argc, char *argv[]) {
 	printf("Big-Int Oblivious Multiplication\n");
+	printf("================================\n");
 
 	if (argc == 4) {
 		const char *remote_host = strtok(argv[1], ":");
@@ -16,8 +17,8 @@ int main (int argc, char *argv[]) {
 
 		printf("Connecting to %s on port %s .. \n", remote_host, port);
 
-		if(argv[2][0] == '1') { 
-      		if(protocolAcceptTcp2P(&pd,port)!=0) { 
+		if(argv[2][0] == '1') {
+      		if(protocolAcceptTcp2P(&pd,port)!=0) {
 				printf("TCP accept from %s failed\n", remote_host);
 				exit(1);
       		}
@@ -28,10 +29,12 @@ int main (int argc, char *argv[]) {
 				exit(1);
 			}
 		}
+		printf("Establishing parties...\n");
 		currentParty = (argv[2][0] =='1'?1:2);
 		setCurrentParty(&pd, currentParty);
 		mul->data = atoi(argv[3]);
 
+		printf("Executing Yao's Protocol...\n");
 		execYaoProtocol(&pd, oblivMul, &mul);
 		cleanupProtocol(&pd);
 
@@ -39,12 +42,11 @@ int main (int argc, char *argv[]) {
 		free(mul);
 	}
 	else {
-		printf("Usage: %s <hostname:port> <1|2> <data>  \n" 
-	    "\tHostname usage:\n" 
+		printf("Usage: %s <hostname:port> <1|2> <data>  \n"
+	    "\tHostname usage:\n"
 		"\tlocal -> 'localhost' remote -> IP address or DNS name\n", argv[0]);
 	}
 
 	return 0;
 
 }
-	
